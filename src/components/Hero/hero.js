edf5f65a81d027246e93a24baf08cf9be8d33639
harshrpg <harshrp94@gmail.com>
2021-06-16 21:05:40 +0100

    Adding Hero content

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,80 @@
+import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { getImage } from "gatsby-plugin-image"
+import { BgImage } from "gbimage-bridge"
+
+const Hero = () => {
+  const { backgroundImage123 } = useStaticQuery(
+    graphql`
+      query {
+        backgroundImage123: file(relativePath: { eq: "hero.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              width: 2000
+              quality: 50
+              webpOptions: { quality: 70 }
+            )
+          }
+        }
+      }
+    `
+  )
+
+  const pluginImage = getImage(backgroundImage123)
+  return (
+    <section class="section">
+      <div class="container hero-banner is-max-desktop has-text-centered ">
+        <div id="hero-banner-text-title">
+          <span class="is-size-1 is-size-3-mobile">
+            World's 1st Token Factory
+          </span>
+        </div>
+        <div id="hero-banner-text-subtitle" class=" has-text-centered">
+          <div class="columns is-gapless">
+            <div class="column ">
+              <span class="is-size-6">
+                The only protocol designed to make you rich. Join the
+                conversation.
+              </span>
+            </div>
+            <div class="column is-one-fifth has-text-left">
+              <button class="button is-light">Join Telegram</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container is-fluid float-image">
+        {/* <StaticImage class="image" src="../../images/hero.png" alt="hero" /> */}
+        {/* <div class="centered">Centered</div> */}
+        {/* <BackgroundImage Tag="section" {...bgImage}>
+          Test
+        </BackgroundImage> */}
+        <BgImage image={pluginImage} className="hero-image">
+          <div class="centered has-text-centered">
+            <div class="columns">
+              <div class="column">
+                <span class="is-size-4">MISSION MARS</span>
+              </div>
+            </div>
+            <div class="columns">
+              <div class="column">
+                <span class="is-size-6">
+                  X2Mars is a new token factory protocol allowing anyone to make
+                  their FOT crypto currency and launch it on Binance Smart
+                  Chain. Read out our whitepaper to know more
+                </span>
+              </div>
+            </div>
+            <div class="columns">
+              <div class="column">
+              <button class="button custom-button">WHITEPAPER</button>
+              </div>
+            </div>
+          </div>
+        </BgImage>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Hero from "../components/Hero/hero"
+
 import "../style/index.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "../style/test.css";
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 const IndexPage = () => {
@@ -13,23 +13,7 @@ const IndexPage = () => {
   return (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <FontAwesomeIcon icon="apple" />
-    <p class="title">Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    /> */}
-    <StaticImage src="../images/hero.png" />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+      <Hero />
   </Layout>
 )}
 
