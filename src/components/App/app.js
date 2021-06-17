51c36b8aa35b5c18d3e539bbc5c7d8aa7fd537a9
harshrpg <harshrp94@gmail.com>
2021-06-17 05:03:23 +0100

    Adding features

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -3,6 +3,7 @@ import * as React from "react"
 import Layout from "../layout"
 import Seo from "../seo"
 import Hero from "../Hero/hero"
+import Features from "../Features/features"
 
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
@@ -14,6 +15,7 @@ const App = props => {
     <Layout>
       <Seo title={props.pageTitle} />
       <Hero />
+      <Features />
     </Layout>
   )
 }
--- src/components/Features/features.js
+++ src/components/Features/features.js
@@ -1,14 +1,32 @@
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
+
+import "./style/features.scss"
 
 const Features = () => {
   return (
-    <section class="hero is-medium is-link">
-      <div class="hero-body">
-        <p class="title">Medium hero</p>
-        <p class="subtitle">Medium subtitle</p>
-      </div>
+    <section class="section is-medium is-fullheight">
+      <span class="floater">
+        <StaticImage class="image" src="../../images/feat.png" />
+      </span>
+      <section class="hero is-medium is-feat">
+        <div class="hero-body">
+          <p class="title is-size-3 is-size-5-mobile">THE TOKEN FACTORY</p>
+          <p class="subtitle is-size-6 is-size-6-mobile">
+            BUILD YOUR NEXT FOT TOKEN WITH EASE
+          </p>
+          <p>
+            Create, Build and Deploy your own Fee-On-Transfer <br /> tokens from our
+            wide range of token types using just a simple form
+          </p>
+        </div>
+      </section>
+      {/* <div class="hero-body">
+        <div class="">
+        </div>
+      </div> */}
     </section>
   )
 }
 
-export default Features;
+export default Features
