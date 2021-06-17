d1034b181da31f8bf0dba32a973f90575e6a1630
harshrpg <harshrp94@gmail.com>
2021-06-17 19:49:16 +0100

    Adding roadmap

--- src/components/Roadmap/roadmap.js
+++ src/components/Roadmap/roadmap.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 import "./style/roadmap.scss"
 
@@ -5,15 +6,20 @@ const RoadMap = () => {
   return (
     <section class="hero is-fullheight is-roadmap">
       <div class="hero-body">
-        <div class="has-text-centered">
-        <p class="title is-size-3 is-size-5-mobile">THE ROADMAP</p>
+        <div class="columns has-text-centered">
+          <div class="column">
+          <p class="title is-size-3 is-size-5-mobile">THE ROADMAP</p>
           <p class="subtitle is-size-6 is-size-6-mobile">
             FIND LATEST EVENTS IN OUR ROADMAP
           </p>
+          </div>
+          <div class="column">
+          <StaticImage class="image" src="../../images/roadmap_1250.png" height="100vh"/>
+          </div>
         </div>
       </div>
     </section>
   )
 }
 
-export default RoadMap;
+export default RoadMap
