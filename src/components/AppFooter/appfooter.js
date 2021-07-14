47245f319eca0287da55398a93a99c17e9f604f4
harshrpg <harshrp94@gmail.com>
2021-07-14 11:59:54 +0100

    Fixing the footer for desktop and mobile

--- src/components/AppFooter/appfooter.js
+++ src/components/AppFooter/appfooter.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from "react"
+import "bulma/css/bulma.css"
+import "@fortawesome/fontawesome-free/css/all.min.css"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import './appfooter.css'
+import "./style/appfooter.scss"
 
 const AppFooter = () => {
-    return(
-        <footer class="footer">
-            <div class="container">
-              <div class="columns is-mobile is-multiline">
-              <div class="column is-half-mobile is-one-quarter-desktop">
-                      <h6 style={{fontWeight: "bolder"}}>X2MARS LLC Corporation</h6>
-                      <p>Terms & Conditions</p>
-                  </div>
+  return (
+    <footer class="footer">
+      <div class="container">
+        <div class="columns is-mobile is-multiline">
+          <div class="column is-full-mobile is-one-quarter-desktop mobile-column">
+            <h6 style={{ fontWeight: "bolder" }}>X2MARS LLC Corporation</h6>
+            <p>Terms & Conditions</p>
+          </div>
 
-                  <div class="column is-half-mobile is-one-quarter-desktop">
-                  <button class="button is-light custom-button app-button">WHITEPAPER</button> 
-                  </div>
+          <div class="column mobile-column">
+            <button class="button is-normal custom-button app-button-footer">
+              WHITEPAPER
+            </button>
+          </div>
 
-                  <div class="column is-half-mobile is-one-quarter-desktop">
-                  <div class="navbar-end">
+          <div class="column is-full-mobile is-one-quarter-desktop social-links mobile-column">
           <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "telegram-plane"]} />
-          </Link>
-          <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "medium-m"]} />
-          </Link>
-          <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "twitter"]} />
-          </Link>
-          <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "discord"]} />
-          </Link>
-          <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "youtube"]} />
-          </Link>
-          <Link to="/" class="navbar-item" activeClassName="navbar-item">
-            <FontAwesomeIcon icon={["fab", "facebook"]} />
-          </Link>
+                <FontAwesomeIcon icon={["fab", "telegram-plane"]} />
+              </Link>
+              <Link to="/" class="navbar-item" activeClassName="navbar-item">
+                <FontAwesomeIcon icon={["fab", "medium-m"]} />
+              </Link>
+              <Link to="/" class="navbar-item" activeClassName="navbar-item">
+                <FontAwesomeIcon icon={["fab", "twitter"]} />
+              </Link>
+              <Link to="/" class="navbar-item" activeClassName="navbar-item">
+                <FontAwesomeIcon icon={["fab", "discord"]} />
+              </Link>
+              <Link to="/" class="navbar-item" activeClassName="navbar-item">
+                <FontAwesomeIcon icon={["fab", "youtube"]} />
+              </Link>
+              <Link to="/" class="navbar-item" activeClassName="navbar-item">
+                <FontAwesomeIcon icon={["fab", "facebook"]} />
+              </Link>
+          </div>
+
+          <div class="column mobile-column">
+            <button class="button is-light custom-button app-button-footer">
+              CONTACT US
+            </button>
+          </div>
         </div>
-                  </div>
-
-                  <div class="column is-half-mobile is-one-quarter-desktop">
-                  <button class="button is-light custom-button app-button">CONTACT US</button>
-                  </div>
-              </div>
-            </div>
-        </footer>
-    )
+      </div>
+    </footer>
+  )
 }
 
 export default AppFooter
