3cc98476eeac2dc9f5e8962f1f8f175d90ac2152
harshrpg <harshrp94@gmail.com>
2021-08-05 07:29:27 +0100

    DO NOT MERGE     [FJ2] - Setting up Connect wallet. 		- Creating custom hook and using context [CLOSED] 		- Moving Web3ReactProvider to AuthProvider and making AuthProvider the global context for interface [CLOSED] 		- Handling balance updates [OPEN] 		- Handling Account switch [OPEN] 		- Handling Chain switch [OPEN] 		- Enable loading screen while connecting [OPEN] 		- Remove connect wallet button on user details retrieved successfully [OPEN]

--- src/context/context.js
+++ src/context/context.js
@@ -1,23 +1,31 @@
-import * as React from "react";
-import { AuthReducer, initialState } from "./reducer";
+import { Web3Provider } from "@ethersproject/providers"
+import { Web3ReactProvider } from "@web3-react/core"
+import * as React from "react"
+import { AuthReducer, initialState } from "./reducer"
 
 const AuthStateContext = React.createContext()
 const AuthDispatchContext = React.createContext()
 
 export function useAuthState() {
-    const context = React.useContext(AuthStateContext)
-    if (context === undefined) {
-        throw new Error("useAuthState must be used within a AuthProvider")
-    }
-    return context
+  const context = React.useContext(AuthStateContext)
+  if (context === undefined) {
+    throw new Error("useAuthState must be used within a AuthProvider")
+  }
+  return context
 }
 
 export function useAuthDispatch() {
-    const context = React.useContext(AuthDispatchContext)
-    if (context === undefined) {
-        throw new Error("useAuthDispatch must be used within a AuthProvider")
-    }
-    return context
+  const context = React.useContext(AuthDispatchContext)
+  if (context === undefined) {
+    throw new Error("useAuthDispatch must be used within a AuthProvider")
+  }
+  return context
+}
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider, "any")
+  library.pollingInterval = 12000
+  return library
 }
 
 export const AuthProvider = ({ children }) => {
@@ -26,7 +34,9 @@ export const AuthProvider = ({ children }) => {
   return (
     <AuthStateContext.Provider value={user}>
       <AuthDispatchContext.Provider value={dispatch}>
-        {children}
+        <Web3ReactProvider getLibrary={getLibrary}>
+          {children}
+        </Web3ReactProvider>
       </AuthDispatchContext.Provider>
     </AuthStateContext.Provider>
   )
--- src/pages/interface.js
+++ src/pages/interface.js
@@ -4,26 +4,17 @@ import Seo from "../components/seo"
 import "../style/index.scss"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
-import { Web3ReactProvider } from "@web3-react/core"
-import { Web3Provider } from "@ethersproject/providers"
 import Factory from "../components/Factory/factory"
 import { AuthProvider } from "../context"
 
-const getLibrary = provider => {
-  const library = new Web3Provider(provider, "any")
-  library.pollingInterval = 12000
-  return library
-}
 const Interface = () => {
   library.add(fab)
   return (
     <AuthProvider>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <AppLayout>
-          <Seo title="App" />
-          <Factory />
-        </AppLayout>
-      </Web3ReactProvider>
+      <AppLayout>
+        <Seo title="App" />
+        <Factory />
+      </AppLayout>
     </AuthProvider>
   )
 }
