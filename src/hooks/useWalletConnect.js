9d1d3d162e11c898e27c33145a2ec03f1473aafe
harshrpg <harshrp94@gmail.com>
2021-09-01 14:22:00 +0100

    [PM-4] - Make Profile Page Functional With Blockchain                     - Fixing Wallet Issues

--- src/hooks/useWalletConnect.js
+++ src/hooks/useWalletConnect.js
@@ -38,6 +38,10 @@ export const useWalletConnect = () => {
           chainId: chainId,
         },
       })
+    } else {
+      dispatch({
+        type: "DISCONNECTED"
+      })
     }
   }, [active, account, chainId, dispatch])
 
