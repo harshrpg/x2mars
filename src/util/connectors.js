374c904767e146937e91f0fe0bdb7cc9259e7a9c
harshrpg <harshrp94@gmail.com>
2021-09-06 18:52:52 +0100

    Removing gatsby-plugin-testing

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,6 +1,6 @@
 import { FortmaticConnector } from "@web3-react/fortmatic-connector"
 import { InjectedConnector } from "@web3-react/injected-connector"
-import { WalletLinkConnector } from "@web3-react/walletlink-connector"
+// import { WalletLinkConnector } from "@web3-react/walletlink-connector"
 import { NetworkConstants } from "./Constants"
 
 // TODO: Remove below infura keys and replace with custom infura keys and put them in .env file
@@ -21,26 +21,25 @@ const injected = new InjectedConnector({
 })
 
 
-const walletlink = new WalletLinkConnector({
-  url: "https://mainnet.infura.io/v3/c3fb6644d68a4d3db5c61fe11905c7dc",
-  appName: "Momentum", // TODO: Update App name here
-  // supportedChainIds: [
-  //   NetworkConstants.MAINNET_ETHEREUM,
-  //   NetworkConstants.ROPSTEN,
-  //   NetworkConstants.RINKEBY,
-  //   NetworkConstants.GOERLI,
-  //   NetworkConstants.KOVAN,
-  //   NetworkConstants.SMART_CHAIN_TESTNET,
-  //   NetworkConstants.SMART_CHAIN_MAINNET,
-  // ],
-  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001]
-})
+// const walletlink = new WalletLinkConnector({
+//   url: "https://mainnet.infura.io/v3/c3fb6644d68a4d3db5c61fe11905c7dc",
+//   appName: "Momentum", // TODO: Update App name here
+//   // supportedChainIds: [
+//   //   NetworkConstants.MAINNET_ETHEREUM,
+//   //   NetworkConstants.ROPSTEN,
+//   //   NetworkConstants.RINKEBY,
+//   //   NetworkConstants.GOERLI,
+//   //   NetworkConstants.KOVAN,
+//   //   NetworkConstants.SMART_CHAIN_TESTNET,
+//   //   NetworkConstants.SMART_CHAIN_MAINNET,
+//   // ],
+//   supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001]
+// })
 
 // TODO: Replace chain id to mainnet. Dynamic Chain Id fetch
 const fortmatic = new FortmaticConnector({apiKey: FORTMATIC_API_KEYS, chainId: NetworkConstants.RINKEBY})
 
 export const Connector = {
     INJECTED: injected,
-    WALLETLINK: walletlink,
     FORTMATIC: fortmatic
 }
